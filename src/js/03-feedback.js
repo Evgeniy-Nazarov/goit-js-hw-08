7397e6f6de86b9f41a6e886015e09f87d761fd33
Evgeniy-Nazarov <perfectyam@gmail.com>
2023-02-02 23:18:35 -0500

    fixed bag

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,41 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const email = document.querySelector("input");
+const message = document.querySelector("textarea");
+
+
+const formData = { email: "", message: "" };
+
+
 form.addEventListener('input', throttle(onFormData, 500));
-form.addEventListener('submit', onSubmitForm);
-const email = document.querySelector('.feedback-form input');
-const message = document.querySelector('.feedback-form textarea');
-
-const formData = {};
-
 function onFormData(e) {
   formData[e.target.name] = e.target.value;
+  console.log(formData);
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-
-
+form.addEventListener('submit', onSubmitForm);
 function onSubmitForm(e) {
   if (email.value === '' || message.value === '') {
-    alert('Заполните пожалуйста все поля формы');
+    alert('Заполните все поля формы!');
     return;
   }
-   e.preventDefault();
+  e.preventDefault();
   e.target.reset();
   localStorage.removeItem('feedback-form-state');
+  
 }
   
 
 
-
-function dataFromLocalStorage() {
-   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  if (localStorage.length !== 0) {   
-    formData.email = data.email;
-    formData.message = data.message;
-    form.value = data.email;
-    form.value = data.message;
-  
+const dataObject = localStorage.getItem('feedback-form-state');
+function onFormLoad(object) {  
+  if (object && email.name === "email") {
+    email.value = JSON.parse(object).email;
+  } if (object && message.name === "message") {
+    message.value = JSON.parse(object).message;
   }
 }
 
-dataFromLocalStorage();
-
-
-
-
+onFormLoad(dataObject);
